64aca878fc0c2ea068a2b2db7477c9613795f079
Aafifa Afaq <ak456fl789@gmail.com>
2024-02-24 20:32:27 +0500

    add

--- main.ts
+++ main.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 console.log("hello World");
 console.log("THANK YOU GOVERNOR SINDH KAMRAN TESSORI");
-=======
-console.log("hello World");
->>>>>>> 73d6bcfc094809f5affa2524d97fa88c0717e565
